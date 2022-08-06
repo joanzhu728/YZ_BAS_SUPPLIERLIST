08a1c95770b3fabfff2b1fb9fb18258896b73dc3
Ying Zhu <ying.zhu@pwc.com>
2022-08-06 02:19:05 +0000

    First commit

---
+++ Component-preload.js
@@ -0,0 +1,7 @@
+//@ui5-bundle customer/supplierlist/Component-preload.js
+jQuery.sap.registerPreloadedModules({
+"version":"2.0",
+"modules":{
+	"customer/supplierlist/i18n/ListReport/C_Supplier/i18n.properties":'#Make sure you provide a unique prefix to the newly added keys in this file, to avoid overriding of SAP Fiori application keys.',
+	"customer/supplierlist/i18n/i18n.properties":'#Make sure you provide a unique prefix to the newly added keys in this file, to avoid overriding of SAP Fiori application keys.\n#XTIT: Application name\ncustomer.supplierlist_sap.app.title=Display Supplier List - Custom'
+}});
